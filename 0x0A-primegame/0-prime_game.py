a84d59e31eee678938c977c3cdc1f50be7422867
Mshanga Makonde <41159327+mshanga@users.noreply.github.com>
2023-09-07 09:11:01 +0300

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,11 +1,60 @@
 #!/usr/bin/python3
 """ Prime Game  """
 
-
 def isWinner(x, nums):
     """ Returns the winner of the Prime Game """
-    if x == 0 or x == -1:
-        return None
-    elif x == 10000:
+    def is_prime(num):
+        if num <= 1:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n == 1:
+            ben_wins += 1
+            continue
+        
+        # Initialize a set to represent the remaining numbers
+        remaining = set(range(2, n + 1))
+
+        maria_turn = True
+        while True:
+            prime_found = False
+            for num in remaining:
+                if is_prime(num):
+                    prime_found = True
+                    prime = num
+                    break
+            if not prime_found:
+                break
+
+            # Remove prime and its multiples
+            remaining.difference_update(range(prime, n + 1, prime))
+
+            if maria_turn:
+                maria_turn = False
+            else:
+                maria_turn = True
+
+        if maria_turn:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
-    return "Ben"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return "None"
+
+# Example usage
+x = 3
+nums = [4, 5, 1]
+winner = isWinner(x, nums)
+print(winner)  # Output: "Ben"
